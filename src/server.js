ff2b388b31ec3743c19e0bd6d84adc5fd839969f
mip <michel@spengergasse.at>
2025-10-20 14:59:02 +0200

    UPDATE Endpoints Prisma ORM Queries

--- src/server.js
+++ src/server.js
@@ -1,19 +1,30 @@
-const express = require("express");
+// const express = require("express");
+import express from "express";
+import { PrismaClient } from '@prisma/client';
+const prisma = new PrismaClient();
+
 const app = express();
 const port = 4000;
 
 // temp data einbinden
-const recipes = require("../assets/temp-data.json");
+// const recipes = require("../assets/temp-data.json");
 
-app.get("/api/recipes", (req, res) => {
+// http://localhost:4000/api/recipes
+app.get("/api/recipes", async (req, res) => {
+  const recipes = await prisma.recipe.findMany({
+    include: { ingredients: true },
+  });
   res.json(recipes);
 });
 
 // GET recipes/id
-app.get("/api/recipes/:id", (req, res) => {
+app.get("/api/recipes/:id", async (req, res) => {
   const id = req.params.id;
 
-  const recipe = recipes.find((recipe) => recipe.id === id)
+  const recipe = await prisma.recipe.findUnique({
+    where: { id: Number(id) },
+    include: { ingredients: true },
+  });
 
   if(!recipe)
     res.status(404).json({message: "Recipe not found!"})
